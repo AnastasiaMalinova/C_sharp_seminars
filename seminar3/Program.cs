fafb16121e504d34b46ddd040ff5eb4256ff4639
Anastasia Malinova <anamali94@mail.ru>
2022-08-27 10:11:14 +0300

    seminar 3 added

---
+++ seminar3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), при чем X != 0 Y !=0 и выдает номр четверти плоскости, в которой находится эта точка
+
+// 1е решение без метода
+// запрашиваем координаты
+Console.WriteLine("Введите координаты x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ( x > 0 && y > 0)
+{
+    Console.WriteLine("Первая четверть");
+}
+else if ( x < 0 && y > 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if ( x < 0 && y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+else ( x > 0 && y < 0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
